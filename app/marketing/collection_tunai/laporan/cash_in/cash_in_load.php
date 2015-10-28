c6fc7c4a30b4cc83e703771bc51704d2e18dfeae
keykurniawan <kurniawan.lucier@gmail.com>
2015-10-29 04:33:09 +0700

    load cash in

--- app/marketing/collection_tunai/laporan/cash_in/cash_in_load.php
+++ app/marketing/collection_tunai/laporan/cash_in/cash_in_load.php
@@ -63,6 +63,7 @@ $difYear = $endYear - $startYear;
 $difMonth = $endMonth - $startMonth;
 
 $array_bulan 		= array(1=>'Januari','Februari','Maret', 'April', 'Mei', 'Juni','Juli','Agustus','September','Oktober', 'November','Desember'); 
+$array_bulan_reverse 		= array('Januari'=>1,'Februari'=>2,'Maret'=>3, 'April'=>4, 'Mei'=>5, 'Juni'=>6,'Juli'=>7,'Agustus'=>8,'September'=>9,'Oktober'=>10, 'November'=>11,'Desember'=>12); 
 $array_bulan[0]		= 'Desember';
 
 $a	= 0;
@@ -119,21 +120,21 @@ if($field1 == 'cb48x')
 /* Pagination */
 
 $query = "
-	SELECT S.POLA_BAYAR, SUM(R.NILAI) AS JUMLAH, DATENAME(MONTH,R.TANGGAL) AS BULAN, DATENAME(YEAR,R.TANGGAL) AS TAHUN
+	SELECT S.POLA_BAYAR, SUM(R.NILAI) AS JUMLAH, DATENAME(MONTH,R.TANGGAL) AS BULAN, MONTH(R.TANGGAL) AS ANGKA_BULAN, DATENAME(YEAR,R.TANGGAL) AS TAHUN, YEAR(R.TANGGAL) AS ANGKA_TAHUN
 	FROM REALISASI R, SPP S 
 	WHERE 
 	R.KODE_BLOK = S.KODE_BLOK AND
 	S.POLA_BAYAR IS NOT NULL
 	$query_search
-	GROUP BY S.POLA_BAYAR, DATENAME(MONTH,R.TANGGAL), DATENAME(YEAR,R.TANGGAL)
+	GROUP BY S.POLA_BAYAR, DATENAME(MONTH,R.TANGGAL), DATENAME(YEAR,R.TANGGAL), MONTH(R.TANGGAL), YEAR(R.TANGGAL)
 	";
 
 	$total_data = $conn->Execute($query);
 	$total_data = $total_data->RecordCount();
 	$total_page = ceil($total_data/$per_page);
 
-$page_num = ($page_num > $total_page) ? $total_page : $page_num;
-$page_start = (($page_num-1) * $per_page);
+	$page_num = ($page_num > $total_page) ? $total_page : $page_num;
+	$page_start = (($page_num-1) * $per_page);
 /* End Pagination */
 ?>
 
@@ -153,26 +154,52 @@ $page_start = (($page_num-1) * $per_page);
 </tr>
 </table>
 
+<?php 
+$data_cash_in = array();
+if ($total_data > 0)
+{	
+	$obj = $conn->selectlimit($query, $per_page, $page_start);
+	$index = 0;
+	while( ! $obj->EOF)
+	{
+		$data_cash_in[$index] = new stdClass();
+		$data_cash_in[$index]->POLA_BAYAR = $obj->fields['POLA_BAYAR'];
+		$data_cash_in[$index]->BULAN = array();
+		for($i=0; $i<=$selisih; $i++) { 
+			list($bulan,$tahun) = explode(' ',$proyeksi_bulan[$i]);
+			$angka_bulan = $array_bulan_reverse[$bulan];
+			if(($angka_bulan.' '.$tahun)==($obj->fields['ANGKA_BULAN'].' '.$obj->fields['TAHUN'])){
+				$data_cash_in[$index]->BULAN[$i] = $obj->fields['JUMLAH'];
+			}else{
+				$data_cash_in[$index]->BULAN[$i] = '-';
+			}
+		}
+		$index++;
+		$obj->movenext();
+	}
+}
+?>
+
 <table class="t-nowrap t-data wm100">
 <tr>
 	<th>NO.</th>
 	<th>POLA PEMBAYARAN</th>
-	<?php for($i=0; $i<=$selisih; $i++) { ?>
+	<?php 
+		for($i=0; $i<=$selisih; $i++) { 
+
+		?>
 	<th><?php echo $proyeksi_bulan[$i];?></th>
 	<?php }?>
 	<th>Total</th>
 </tr>
 
 <?php
-	$result = array();
-	$num_fields = mysql_num_fields($query);
-	$num_row = mysql_num_rows($query);
 
 if ($total_data > 0)
 {
-	$sum_nilai=0;
-
-	$obj = $conn->selectlimit($query, $per_page, $page_start);
+	$sum_nilai=array();
+	$sum_kolom = array();
+	
 	$i = 1 + $page_start;
 	$sub_unit					= 1;
 	$sub_total_harga 			= 0;
@@ -181,40 +208,46 @@ if ($total_data > 0)
 	$sub_nilai_proyeksi[0]		= 0;
 	$sub_total_proyeksi			= 0;
 
-	while( ! $obj->EOF)
-	{
-		$POLA_BAYAR = $obj->fields['POLA_BAYAR'];
-		$JUMLAH = $obj->fields['JUMLAH'];
-		
+	foreach($data_cash_in as $index=>$data){
+	$sum_baris = 0;
+
 		?>
 		<tr class="onclick" id="<?php echo $id; ?>"> 
 			<td class="text-center"><?php echo $i; ?></td>
-			<td><?php echo $POLA_BAYAR; ?></td>
+			<td><?php echo $data->POLA_BAYAR; ?></td>
 
-			<?php for($j=0; $j<=$selisih; $j++) { 
-				$nilai = $JUMLAH;
-				$sum_nilai += $nilai ;
+			<?php foreach($data->BULAN as $key=>$jumlah) { 
+				$nilai = ($jumlah!='')?$jumlah:0;
+				$sum_kolom[$index][$key] = ($jumlah!='-')?$jumlah:0;
+				$sum_baris += $nilai ;
 				?>
-			<td class="text-center"><?= to_money($nilai) ?></td>
-			<?php }?>
+			<td class="text-center"><?= ($jumlah!='-')?to_money($jumlah):'-'; ?></td>
+			<?php }
+			array_push($sum_nilai,$sum_baris);
+			?>
 			
-			<td class="text-center"><?php echo to_money($sum_nilai); ?></td>
+			<td class="text-center"><?php echo to_money($sum_baris); ?></td>
 		</tr>
 		<?php
 		$i++;
-		$obj->movenext();
-	}	
+	}
 	?>
 	
 	<tr> 
 		<td class="text-center"></td>
 		<td class="text-center"><b>TOTAL</b></td>
 		<?php 
-			for($k=0; $k<=$selisih; $k++){?>
-				<td class="text-center"><b><?php echo to_money($sum_nilai); ?></b></td>
+
+			for($k=0; $k<=$selisih; $k++){
+				$sum_total_kolom = 0;
+				foreach($sum_kolom as $key=>$val){
+					$sum_total_kolom+= $val[$k];
+				}
+				?>
+				<td class="text-center"><b><?php echo to_money($sum_total_kolom); ?></b></td>
 			<?php }
 		?>
-		<td class="text-center"><b><?php echo to_money($sum_nilai); ?></b></td>
+		<td class="text-center"><b><?php echo to_money(array_sum($sum_nilai)); ?></b></td>
 <?php	
 }
 ?>
