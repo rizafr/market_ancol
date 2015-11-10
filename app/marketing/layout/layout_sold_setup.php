f6869ad1d80155e563b0e183abe52cfb23d699ef
rizafr <riza.fauzi.rahman@gmail.com>
2015-11-10 16:43:03 +0700

    perbaikan layout sold menjadi matriks

--- app/marketing/layout/layout_sold_setup.php
+++ app/marketing/layout/layout_sold_setup.php
@@ -1,36 +1,212 @@
-<html>
-<head>
+<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">    
+<link rel="stylesheet" href="../config/css/antrian/bootstrap.css">
+<link rel="stylesheet" href="../config/css/antrian/frontend.css">
+<link rel="stylesheet" href="../config/css/antrian/font-awesome.css">
+<link rel="stylesheet" href="../config/css/antrian/hover-min.css">
+
 <style type="text/css">
-#myiframe {border: 0; position:fixed; top:0; left:0; right:0; bottom:0; width:100%; height:100%; z-index:999999999;} 
-</style>
-</head>
-<body>
-<div id="scroller">
+
+
+    /* make keyframes that tell the start state and the end state of our object */
+    @-webkit-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+    @-moz-keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+    @keyframes fadeIn { from { opacity:0; } to { opacity:1; } }
+
+    b{
+        text-align:center;
+        background-color : #006064;
+        color: #FFFFF2;
+    }
+
+    td{
+        padding-right: 2px!important; text-align: center !important;
+        vertical-align: middle  !important;
+        line-height: 10px  !important;  
+        font-size: 14px;
+        border: 1px solid !important;}
+
+    </style>
+</head> 
+
 <?php
+$data_sort = array();
+for ($a = 0; $a <= 36; $a++) {
+    $data_sort[$a] = array();
+    for ($b = 0; $b <= 22; $b++) {
+        $data_sort[$a][$b] = 'x';
+    }
+}
 
-$file = '../files/layout_sold.pdf';
-//$file = '../files/upload.xlsx';
-$filename = 'layout_sold.pdf'; /* Note: Always use .pdf at the end. */
-if(file_exists($file)) {
-	
-// header('Content-type: application/pdf');
-// header('Content-Disposition: inline; filename="' . $filename . '"');
-// header('Content-Transfer-Encoding: binary');
-// header('Content-Length: ' . filesize($file));
-// header('Accept-Ranges: bytes');
+$limitPerLine = 22;
+$currentData = 0;
 
-// @readfile($file);
+$query = "
+SELECT  
+s.KODE_BLOK,
+s.STATUS_STOK,
+s.TERJUAL,
+t.TIPE_BANGUNAN,
+LOKASI
+FROM 
+STOK s
+LEFT JOIN TIPE t ON s.KODE_TIPE = t.KODE_TIPE
+LEFT JOIN HARGA_SK hs ON s.KODE_SK = hs.KODE_SK AND s.KODE_BLOK = hs.KODE_BLOK
+LEFT JOIN LOKASI h ON s.KODE_LOKASI = h.KODE_LOKASI
+ORDER BY s.NO_VA ASC
+";
 
-echo "<iframe name=\"myiframe\" id=\"myiframe\" src=\"$file\" width=\"100%\" style=\"height:100%\"></iframe>";
+$obj        = $conn->execute($query);
+while( ! $obj->EOF)
+{       
 
-
+ $kode_blok = $obj->fields['KODE_BLOK'];
+ $blok = explode("-", $kode_blok);
+ $no_unit = (int) $blok[1];
+ $jml= strlen($blok[0]);
+ if($jml>2){
+    $tower = substr($blok[0], 0,1);
+    $lantai = substr($blok[0], 1,2);
 }else{
-	echo "Tidak ada";
+    $tower = substr($blok[0], 0,1);
+    $lantai = substr($blok[0], 1,3);    
+}
+
+if ($obj->fields['STATUS_STOK'] == '0' AND $obj->fields['TERJUAL'] == '0'){
+    $status = 'STOK BELUM SIAP JUAL';
+    $data_sort[$lantai][$no_unit] = 'Avail';
+}else
+if ($obj->fields['STATUS_STOK'] == '1' AND $obj->fields['TERJUAL'] == '0'){
+    $status = 'STOK SUDAH SIAP JUAL';
+    $data_sort[$lantai][$no_unit] = 'Avail';
+}else 
+if ($obj->fields['STATUS_STOK'] == '1' AND $obj->fields['TERJUAL'] == '1'){
+    $status = 'STOK SUDAH DI RESERVE';
+    $data_sort[$lantai][$no_unit] = 'Sold';
+}else 
+if ($obj->fields['STATUS_STOK'] == '1' AND $obj->fields['TERJUAL'] == '2'){
+    $status = 'STOK SUDAH TERJUAL';
+    $data_sort[$lantai][$no_unit] = 'Sold';
+}
+$obj->movenext();
+
 }
 
 ?>
+<div class="row">
+    <div class="col-md-6">
+        <table class="table" >					
+            <tbody >
+                <?php
+                echo "<tr><td class = 'active' colspan='10'>Ancol View</td>";
+                echo "<tr><td class = 'active'>LT</td>";
 
+                for ($b = 22; $b >= 1; $b-=2) {
+                    if ($b == 13 || $b == 14 || $b == 4) {
+                        continue;
+                    }
+                    echo "<td class='active'>$b</td>";
+                }
+
+                for ($a = 36; $a > 0; $a--) {
+                    if ($a == 13 || $a == 4 || $a == 14 || $a == 24 || $a == 34) {
+                        continue;
+                    }
+                    echo "<tr><td class='active'>$a</td>";
+                    for ($b = 22; $b >= 1; $b-=2) {
+                        if ($b == 13 || $b == 14 || $b == 4) {
+                            continue;
+                        }
+                        if ($data_sort[$a][$b] == 'Sold') {
+                            echo "<td class = 'danger'>A$a-$b</td>";
+                        } else if ($data_sort[$a][$b] == 'Avail') {
+                            echo "<td class = 'success'>A$a-$b</td>";
+                        } else if ($data_sort[$a][$b] == 'Booked') {
+                            echo "<td class = 'danger'>A$a-$b</td>";
+                        } else {
+                            if ($a >= 2) {
+                                if ($data_sort[$a - 1][$b] == 'x') {
+                                    echo "<td class = 'active'>Void</td>";
+                                } else {
+                                    echo "<td class = 'active'>Garden</td>";
+                                }
+                            } else {
+                                echo "<td class = 'active'>Garden</td>";
+                            }
+                        }
+                    }
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>	
+    </div>
+
+    <div class="col-md-6">
+        <table class="table" >					
+            <tbody >
+                <?php
+                echo "<tr><td class = 'active' colspan='11'>Sea View</td>";
+                echo "<tr><td class = 'active'>LT</td>";
+
+                for ($b = 21; $b >= 1; $b-=2) {
+                    if ($b == 13) {
+                        continue;
+                    }
+                    echo "<td class='active'>$b</td>";
+                }
+
+                for ($a = 36; $a > 0; $a--) {
+                    if ($a == 13 || $a == 14 || $a == 4 || $a == 24 || $a == 34) {
+                        continue;
+                    }
+                    echo "<tr>
+                    <td class='active'>$a</td>";
+                    for ($b = 21; $b >= 1; $b-=2) {
+                        if ($b == 13 || $b == 14 || $b == 4) {
+                            continue;
+                        }
+                        if ($data_sort[$a][$b] == 'Sold') {
+                            echo "<td class = 'danger'>A$a-$b</td>";
+                        } else if ($data_sort[$a][$b] == 'Avail') {
+                            echo "<td class = 'success'>A$a-$b</td>";
+                        } else if ($data_sort[$a][$b] == 'Booked') {
+                            echo "<td class = 'danger'>A$a-$b</td>";
+                        } else {
+                            if ($a >= 2) {
+                                if ($data_sort[$a - 1][$b] == 'x') {
+                                    echo "<td class = 'active'>Void</td>";
+                                } else {
+                                    echo "<td class = 'active'>Garden</td>";
+                                }
+                            } else {
+                                echo "<td class = 'active'>Garden</td>";
+                            }
+                        }
+                    }
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
 </div>
-</body>
-</html>
+<div class="row">
+    <div class="col-md-2">
+        Ket : 
+        <table class="table">
+            <tr>
+                <td class="success">
+                    Marketable
+                </td>
+                <td class="danger">
+                    SOLD
+                </td>
+                <td class="active">
+                    SKY GARDEN
+                </td>
+            </tr>
+        </table>
+
+    </div>
+</div>
 
