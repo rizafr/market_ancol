366bc4332d7889bf67bb47b976e41cae98fc0634
waykanza <apriant.wahyu@gmail.com>
2015-11-11 17:53:18 +0700

    umur piutang

--- app/marketing/collection_tunai/laporan/umur_piutang/umur_piutang_load.php
+++ app/marketing/collection_tunai/laporan/umur_piutang/umur_piutang_load.php
@@ -59,12 +59,6 @@ SELECT
 	COUNT(*) AS TOTAL
 FROM
 	SPP a 
-	LEFT JOIN STOK b ON a.KODE_BLOK = b.KODE_BLOK
-	LEFT JOIN TIPE c ON b.KODE_TIPE = c.KODE_TIPE
-	LEFT JOIN HARGA_TANAH d ON b.KODE_SK_TANAH = d.KODE_SK
-	LEFT JOIN HARGA_BANGUNAN e ON b.KODE_SK_BANGUNAN = e.KODE_SK
-	LEFT JOIN FAKTOR f ON b.KODE_FAKTOR = f.KODE_FAKTOR
-	LEFT JOIN CS_VIRTUAL_ACCOUNT g ON a.NOMOR_CUSTOMER = g.NOMOR_VA	
 $query_search
 ";
 
@@ -97,8 +91,12 @@ $page_start = (($page_num-1) * $per_page);
 	<th rowspan="2">BLOK / NOMOR</th>
 	<th rowspan="2">NAMA PEMBELI</th>
 	<th rowspan="2">TOTAL TRANSAKSI</th>
+	<th rowspan="2">TOTAL BELUM JATUH TEMPO</th>
+	<th rowspan="2">TOTAL SUDAH JATUH TEMPO</th>
 	<th colspan="2">RENCANA BAYAR</th>
-	<th colspan="3">UMUR TAGIHAN SUDAH JATUH TEMPO</th>
+	<th colspan="7">UMUR TAGIHAN SUDAH JATUH TEMPO</th>
+	<th rowspan="2">CARA PEMBAYARAN</th>
+	<th rowspan="2">CATATAN</th>
 </tr>
 <tr>
 	<th colspan="1">Tanggal</th>
@@ -106,21 +104,19 @@ $page_start = (($page_num-1) * $per_page);
 	<th colspan="1">0 to 30</th>
 	<th colspan="1">30 to 60 Hari</th>
 	<th colspan="1">60 to 90</th>
+	<th colspan="1">90 to 120</th>
+	<th colspan="1">120 to 360</th>
+	<th colspan="1">360 to 720</th>
+	<th colspan="1">OV 720</th>
 </tr>
 
 <?php
 if ($total_data > 0)
 {
 	$query = "
-	SELECT *
+	SELECT KODE_BLOK,NAMA_PEMBELI,HARGA_TOTAL
 	FROM
 		SPP a 
-		LEFT JOIN STOK b ON a.KODE_BLOK = b.KODE_BLOK
-		LEFT JOIN TIPE c ON b.KODE_TIPE = c.KODE_TIPE
-		LEFT JOIN HARGA_TANAH d ON b.KODE_SK_TANAH = d.KODE_SK
-		LEFT JOIN HARGA_BANGUNAN e ON b.KODE_SK_BANGUNAN = e.KODE_SK
-		LEFT JOIN FAKTOR f ON b.KODE_FAKTOR = f.KODE_FAKTOR
-		LEFT JOIN CS_VIRTUAL_ACCOUNT g ON a.NOMOR_CUSTOMER = g.NOMOR_VA		
 		$query_search
 	";
 	
@@ -130,27 +126,8 @@ if ($total_data > 0)
 
 	while( ! $obj->EOF)
 	{
-		$id 				= $obj->fields['KODE_BLOK'];
-		$luas_tanah 		= $obj->fields['LUAS_TANAH'];
-		$luas_bangunan 		= $obj->fields['LUAS_BANGUNAN'];
-		
-		$tanah 				= $luas_tanah * ($obj->fields['HARGA_TANAH']) ;
-		$disc_tanah 		= round($tanah * ($obj->fields['DISC_TANAH'])/100,0) ;
-		$nilai_tambah		= round(($tanah - $disc_tanah) * ($obj->fields['NILAI_TAMBAH'])/100,0) ;
-		$nilai_kurang		= round(($tanah - $disc_tanah) * ($obj->fields['NILAI_KURANG'])/100,0) ;
-		$faktor				= $nilai_tambah - $nilai_kurang;
-		$total_tanah		= $tanah - $disc_tanah + $faktor;
-		$ppn_tanah 			= round($total_tanah * ($obj->fields['PPN_TANAH'])/100,0) ;
-		
-		$bangunan 			= $luas_bangunan * ($obj->fields['HARGA_BANGUNAN']) ;
-		$disc_bangunan 		= round($bangunan * ($obj->fields['DISC_BANGUNAN'])/100,0) ;
-		$total_bangunan		= $bangunan - $disc_bangunan;
-		$ppn_bangunan 		= round($total_bangunan * ($obj->fields['PPN_BANGUNAN'])/100,0) ;
-		
-		$total_harga 		= to_money($total_tanah + $total_bangunan);
-		$total_ppn			= to_money($ppn_tanah + $ppn_bangunan);
-		
-		$total_harga		= ($total_tanah + $total_bangunan) + ($ppn_tanah + $ppn_bangunan);	
+		$id 				= $obj->fields['KODE_BLOK'];		
+		$total_harga 		= $obj->fields['HARGA_TOTAL'];
 		
 		$sub_nilai 	= 0;
 		$sub_45		= 0;
